75413a711ab2089e6e167c04fd184d42f705c78d
Adán Mauri Ungaro <adan.mauri@gmail.com>
2026-07-01 16:39:50 -0300

    Agrega long_description a setup.py leyendo el README
    Sin esto PyPI mostraba la descripción como UNKNOWN al no tener
    long_description configurado.

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import os
 from distutils.core import setup
+
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, '..', 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='FrameDuino',
     version='0.2',
     description='Pinguino Board Framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Adán Mauri Ungaro',
     author_email='adan.mauri@gmail.com',
     url='https://github.com/adanmauri/frameduino',
     packages=['frameduino', 'frameduino.drivers'],
-    license='GPLv3'    
+    license='GPLv3'
 )
